e317b1ff995cf77a85f4a2b6d9e75e902bcbbdd6
Kelvin Carvalho <kelvin.carvalho@mercadolivre.com>
2023-01-19 13:53:13 -0300

    fix: endpoint

--- src/main/java/com/example/flows/controller/HelloController.java
+++ src/main/java/com/example/flows/controller/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/demo")
 public class HelloController {
 
   private ServiceExample serviceExample;
@@ -18,13 +20,13 @@ public class HelloController {
     this.serviceExample = serviceExample;
   }
 
-  @PostMapping(value = "/test")
+  @PostMapping(value = "/post")
   public ResponseExampleDTO postExample(@RequestBody PostExampleDTO body) {
     return this.serviceExample.postExample();
   }
 
 
-  @PutMapping(value = "/test/{id}")
+  @PutMapping(value = "/put/{id}")
   public ResponseExampleDTO putExample(@RequestBody PostExampleDTO body, @PathVariable Long id) {
     return this.serviceExample.putExample(id);
   }
